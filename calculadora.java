8b12302b0d65e097f37ef764ee0d718b4053eb19
Juan Ignacio Herrero Martínez <juanignacioherrerom@gmail.com>
2016-09-21 13:57:29 +0200

    Se añade la programacion y la interfaz para numeros romanos

--- calculadora.java
+++ calculadora.java
@@ -5,11 +5,16 @@ import modulo.Calculo;
 
 public class calculadora {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {        
+        
+        //Añadir una opcion para preguntar si el usuario quiere introducir números romanos,
+        //ejecutar el método de la clase numerosRomanos para la conversión
+        
         Calculo calculo = new Calculo(pedir(), pedir());
         System.out.println("La suma es: " + calculo.Sumar());
         comprobarResta(calculo);
         System.out.println("La multiplicacion es: " + calculo.Multiplicar());
+        
     }
 
     public static int pedir() {
---
+++ numerosRomanos.java
@@ -0,0 +1,9 @@
+
+package modulo;
+
+public class numerosRomanos {
+    
+    //Contiene como constante un Array con las equivalencias entre números romanos y decimales.
+    
+    //Contiene un método que busca en el array el número introducido y lo convierte en decimal.
+}
