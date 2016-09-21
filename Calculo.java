ac83b7d6c065267c1a450ddf657116fd54f5c889
Juan Ignacio Herrero Martínez <juanignacioherrerom@gmail.com>
2016-09-21 13:30:41 +0200

    Metodo de suma

---
+++ Calculo.java
@@ -0,0 +1,17 @@
+
+package modulo;
+
+public class Calculo {
+    int a;
+    int b;
+
+    public Calculo(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    
+    public int Suma() {
+    return a+b;
+    }
+        
+}
