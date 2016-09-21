5a13ecf955fdd7029b3d5bb8e161e4e05ef6c400
Juan Ignacio Herrero Martínez <juanignacioherrerom@gmail.com>
2016-09-21 13:43:43 +0200

    Elaboracion del metodo fallo en la resta en otra rama

--- calculadora.java
+++ calculadora.java
@@ -1,22 +1,30 @@
-
 package principal;
 
 import java.util.Scanner;
 import modulo.Calculo;
 
-
 public class calculadora {
 
-  
     public static void main(String[] args) {
-      Calculo calculo = new Calculo(pedir(),pedir());
-        System.out.println("La suma es: "+calculo.Sumar());
+        Calculo calculo = new Calculo(pedir(), pedir());
+        System.out.println("La suma es: " + calculo.Sumar());
+                comprobarResta(calculo);
     }
-    
-    public static int pedir (){
-        Scanner sc = new Scanner (System.in);
+
+    public static int pedir() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Introduzca un número: ");
         int num = Integer.parseInt(sc.nextLine());
-    return num;
+        return num;
+    }
+    
+    public static void comprobarResta (Calculo calculo){
+        int resta;
+        resta = calculo.Restar();
+    if (resta>=0) {
+        System.out.println("La resta es: "+resta);
+    } else {
+        System.out.println("El resultado no es un número natural, por favor introduzca otros valores");
+    }
     }
 }
